2a54d247e7c84dd2c5ca75e6380cbc935bf4db20
Jo-info <weblearning085@gmail.com>
2025-01-12 16:33:12 +0000

    Add files via upload
    My website

---
+++ Exo 1(codage).py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 15:18:55 2025
+
+@author: INTEL
+"""
+
+a=2
+b=-3.1
+c='hello'
+print(type(a))
+print(type(b))
+print(type(c))
+
+X='varible1'
+print(X)
+
+X='42'
+print(int(X))
+
+J='20.0'
+print(float(J))
+
+X=20   # X est un entier naturel
+Y='20' # Y est une chaine de caractère
+Z=20.0 # Z est un nombre réel
+print(type(X))
+print(type(Y))
+print(type(Z))
+print('hi') #essai
